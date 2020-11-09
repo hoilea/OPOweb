b8ee7a5bccd0cb80676f7b78698f0b023ed62b9c
hoilea <ghltjr0320@naver.com>
2020-11-09 15:16:52 +0900

    ㅇㅁㅈㅇㄴㅇ

---
+++ app/src/main/java/com/example/newwebapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.newwebapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl("http://onepleone.com/")
+
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack()
+        }else{
+            super.onBackPressed()
+        }
+    }
+
+}
